2ab83db606c5ae5ed3317f81131bd044b75045cf
Vitaliy <klaot@yandex.ru>
2022-09-23 11:43:59 +0300

    закончил верстку основного функционала.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Home from './page/Home';
+import Header from './components/Header';
+
 
 
 function App() {
   return (
     <div className='container'>
-      <Home />
+      <Header />
     </div>
   );
 }
