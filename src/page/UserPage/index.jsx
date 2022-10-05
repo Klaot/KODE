cffa280c5c63418fad03ea0e8a661e7d4a7d57e1
Vitaliy <klaot@yandex.ru>
2022-10-06 00:59:00 +0300

    сделал отображение полного возраста и даты рожения на страницы работника

--- src/page/UserPage/index.jsx
+++ src/page/UserPage/index.jsx
@@ -8,12 +8,13 @@ import { useSelector } from 'react-redux';
 function UserPage() {
 
   const user =  useSelector(state => state.userItem);
-  
+  let foo = user.birthday.split('-');
+
   return (
     <div className={styles.userPage}>
       <div className={styles.headerInfo}>
         <div className={styles.userInfo}>
-          <img src={user.avatarUrl} />
+          <img src={user.avatarUrl} alt='avatarUrl icon'/>
           <div className={styles.userName}>
              <h1>{user.firstName} {user.lastName}</h1>
              <p>{user.userTag === 'string' ? '' : user.userTag}</p>
@@ -23,14 +24,18 @@ function UserPage() {
       </div>
 
      <div className={styles.userNumberAndBirthday}>
-      <div>
+      <div className={styles.age}>
         <div className={styles.userBirthday}>
           <img src={StarIcon} alt='star-icon'/>
-          <h4>{user.birthday}</h4>
+          <h4>{new Date(foo[0], foo[1], foo[2]).toLocaleString('ru' , {
+                year: 'numeric',
+                month: 'long',
+                day: 'numeric'
+              })}
+          </h4>
         </div>
-        <p></p>
+        <p>Возраст: { new Date().getFullYear() - user.birthday.slice(0,4) }</p>
       </div>
-      
       <div className={styles.userNumber}>
         <img src={PhoneIcon} alt='phone-icon'/>
         <h4>{user.phone}</h4>
