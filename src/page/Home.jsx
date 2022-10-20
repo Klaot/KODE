783f99337c7317d2f5f2f0ab9369bb4fb87d7025
Vitaliy <klaot@yandex.ru>
2022-10-21 01:06:56 +0300

    добавил комментарии для понимания хоть чего то =)

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -15,12 +15,12 @@ const skeletonList = [1,2,3,4,5,6]
 
 function Home() {
 
-  const { categoryId, checkbox  } = useSelector(state => state.filter);
-  const { searchValue } = useSelector(state => state.search);
-  const [ users, setUsers ] = useState([])
-  const [ isLoading, setIsLoading ] = useState(true)
-  const [ isError , setIsError ] = useState(false)
-  const debouncedSearchTerm = useDebounce(searchValue, 1000)
+  const { categoryId, checkbox  } = useSelector(state => state.filter); // Выбранная категория и способ сортировки.
+  const { searchValue } = useSelector(state => state.search); // Состояние строки поиска.
+  const [ users, setUsers ] = useState([]) // Массив с пользователями.
+  const [ isLoading, setIsLoading ] = useState(true) // Состояние загрузки.
+  const [ isError , setIsError ] = useState(false)  // Состояние ошибки
+  const debouncedSearchTerm = useDebounce(searchValue, 1000) // Функция отвечающия за задержку запроса при вводе в строку поиска
 
   
   // Использую фильтрацию на стороне сервера. Если бы по запросу возвращалось очень большое количество 
@@ -47,6 +47,7 @@ function Home() {
 
 
   //Фильтрация по поиску.
+
   let filteredName =  users.filter(item => {
     return item.firstName.toLowerCase().includes(debouncedSearchTerm.toLowerCase())
   })
@@ -57,10 +58,14 @@ function Home() {
         <Header />
         <div className='all-users'>
           { 
-            checkbox === 0 ? isError ? <ErrorComponent setIsError = {setIsError}/> : isLoading ? skeletonList.map((item, index) => <Skeleton key={index} />) :
+            checkbox === 0 ? // Если выбрана сортировка по алфавиту
+            isError ? <ErrorComponent setIsError = {setIsError}/> : // Проверка ошибки
+            isLoading ? skeletonList.map((item, index) => <Skeleton key={index} />) : // Если еще идет загрузка показываем скелетон.
             filteredName.length === 0 ? <UserNotFound /> : filteredName.map((item, index) => {
-              return <UsersList key={index} index={index} {...item}/>
-            }) : checkbox === 1 ? isError ? <ErrorComponent setIsError = {setIsError}/> : isLoading ? skeletonList.map((item, index) => <Skeleton key={index} />) :
+              return <UsersList key={index} index={index} {...item}/> 
+            }) : checkbox === 1 ? // Если выбрана сортировка по дате рождения
+            isError ? <ErrorComponent setIsError = {setIsError}/> : // Проверка ошибки
+            isLoading ? skeletonList.map((item, index) => <Skeleton key={index} />) :// Если еще идет загрузка показываем скелетон.
             filteredName.length === 0 ? <UserNotFound /> : <UserListSortData filteredName={filteredName}/> : null
           }
         </div> 
